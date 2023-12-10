d3309edfe96f30a9c585bc0b482f45416fa54009
MartaSamoilenkoPn <samoilenko.pn@ucu.edu.ua>
2023-12-10 20:48:47 +0200

    structure

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,10 +2,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LabApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(LabApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
